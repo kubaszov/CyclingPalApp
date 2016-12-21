2a0920d96b475219b22ba66fe4d5bf124ae7bda0
David Szentesi <david.sentes@gmail.com>
2016-12-21 23:18:32 +0000

    Implemented HomeScreenFragment1 on HomeScreenActivity

--- app/src/main/java/com/szentesi/david/cyclingpalapp/HomeScreenActivity.java
+++ app/src/main/java/com/szentesi/david/cyclingpalapp/HomeScreenActivity.java
@@ -1,13 +1,31 @@
 package com.szentesi.david.cyclingpalapp;
 
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 
-public class HomeScreenActivity extends AppCompatActivity {
+public class HomeScreenActivity extends AppCompatActivity implements HomeScreenFragment1.OnFragmentInteractionListener {
 
+    // to implement fragment need to do the following
+    // http://stackoverflow.com/questions/24777985/how-to-implement-onfragmentinteractionlistener
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        // calling HomeScreenFragment1
+        // http://stackoverflow.com/questions/5159982/how-do-i-add-a-fragment-to-an-activity-with-a-programmatically-created-content-v
+        if(savedInstanceState == null) {
+            Fragment newFragment = new HomeScreenFragment1();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.activity_home_screen, newFragment).commit();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
